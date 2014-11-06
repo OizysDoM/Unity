cbb3457d53e01e5f073d33ae73a56091cdc94e9c
SveinEven <sveineven.jo@gmail.com>
2014-11-06 13:06:27 +0100

    Astronaut

--- Assets/Scripts/ItemTeller.js
+++ Assets/Scripts/ItemTeller.js
@@ -1,7 +1,7 @@
 ﻿#pragma strict
 private var teller : int;
 private var charTeller : CharTeller;
-charTeller = GameObject.Find("Sphere").GetComponent(CharTeller);
+charTeller = GameObject.Find("Graphics").GetComponent(CharTeller);
 private var object = GameObject;
 
 
---
+++ Assets/Scripts/PlayerAnimate.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+function Start () {
+
+}
+
+function Update () {
+	var player = GameObject.Find("First Person Controller").GetComponent(CharacterMotor);
+	var AT = gameObject.GetComponent(AnimateTexture);
+	if (Input.GetKey("a")){
+		AT.rowNumber = 2;
+	} else if(Input.GetKey("d")){
+		AT.rowNumber = 2;
+	} else if(Input.GetKey("w")){
+		
+	}else{
+		AT.rowNumber = 1;
+	}
+	if (!player.grounded){
+		Debug.Log("tst");
+		AT.rowNumber = 0;
+	}
+
+}
